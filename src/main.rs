642e7da227fd7cbe2f23b1b8cc737bd8c2c13116
cpf2021-gif <2992247892@qq.com>
2023-07-31 16:56:11 +0800

    solve day5

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod aoc;
 
 fn main() {
-    println!("{}", aoc::day4::solve_part1("input/day4/input.txt"));
-    println!("{}", aoc::day4::solve_part2("input/day4/input.txt"));
+    println!("{}", aoc::day5::solve_part1("input/day5/input.txt"));
+    println!("{}", aoc::day5::solve_part2("input/day5/input.txt"));
 }
