e95b888b721ca7251bfa898b77bb9b96c05f9f9f
cpf2021-gif <2992247892@qq.com>
2023-08-01 15:15:28 +0800

    test pre-commit-hook

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod aoc;
 
 fn main() {
+    // test pre-commit hook
     println!("{}", aoc::day7::solve_part1("input/day7/input.txt"));
     println!("{}", aoc::day7::solve_part2("input/day7/input.txt"));
 }
